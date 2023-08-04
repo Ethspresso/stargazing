3d8215ce485c07ba8d75bbdb2c277dbb78b33006
Ethspresso <ethspresso@gmail.com>
2023-08-04 13:44:59 +0200

    Big update to draw using an offscreen buffer

--- project/src/index.js
+++ project/src/index.js
@@ -4,22 +4,23 @@ import p5 from 'p5';
 let start;
 let width;
 let height;
+let buffer;
+let buffer_width;
+let fullscreen_ratio;
 let palette;
 let palettes;
-let arcs;
-let star;
-let max_arch_iter;
-let max_star_iter;
 let point_count;
 let early_stop;
 
 // Star options
+let star;
 let star_repeat;
 let star_ray_len;
 let star_point_count;
 let star_size_px;
 let star_size_px_min;
 let star_size_px_max;
+let max_star_iter;
 
 // Beam options
 let arc_count;
@@ -27,6 +28,7 @@ let arc_point_count;
 let start_off;
 let end_x_off;
 let end_y_off;
+let max_arch_iter;
 let max_point_amplitude;
 let twistyness;
 
@@ -60,18 +62,15 @@ let sketch = function (p5) {
     let paletteNames = Object.keys(palettes);
     palette = paletteNames[Math.floor(fxrand() * paletteNames.length)];
 
-    // Determine features
-    arcs = true;
-    star = fxrand() < 0.8 ? true : false;
-
-    // Arc features
+    // Determine beam features
     let arc_counts = [4, 4, 8, 16, 24, 32, 32, 48, 64, 64, 128, 128];
     arc_count = arc_counts[Math.floor(fxrand() * arc_counts.length)];
     let arc_point_options = [1024, 2048, 3072, 4096];
     arc_point_count = arc_point_options[Math.floor(p5.map(fxrand(), 0, 1, 0, arc_point_options.length))];
     max_point_amplitude = window.innerWidth / 60;
 
-    // Star features
+    // Determine star features
+    star = fxrand() < 0.8 ? true : false;
     let star_repeat_opts = [128, 256, 512];
     star_repeat = star_repeat_opts[Math.floor(fxrand() * star_repeat_opts.length)];
     let star_ray_len_opts = [0, 0.05, 0.1, 0.15, 0.2, 0.25];
@@ -137,14 +136,22 @@ let sketch = function (p5) {
 
     width = window.innerWidth;
     height = window.innerHeight;
-    p5.createCanvas(width, height, p5.WEBGL);
+    p5.createCanvas(width, height);
     console.log('Rendering at', width, 'x', height);
 
-    p5.colorMode(p5.HSB, 360, 100, 100, 1.0);
-    p5.setAttributes('antialias', true);
+    // Create a buffer to draw everything to
+    // Use fixed width and flexible height based on current aspect ratio,
+    // so that when we draw the buffer it appears to be fullscreen
+    buffer_width = 1200;
+    fullscreen_ratio = height/width;
+    buffer = p5.createGraphics(buffer_width, Math.floor(buffer_width*fullscreen_ratio), p5.WEBGL);
+    console.log('Created buffer at', buffer.width, 'x', buffer.height);
+
+    buffer.colorMode(p5.HSB, 360, 100, 100, 1.0);
+    buffer.setAttributes('antialias', true);
     p5.frameRate(60);
     // This helps with rotation logic
-    p5.angleMode(p5.DEGREES);
+    buffer.angleMode(p5.DEGREES);
 
     // Iteration details to console
     for (const key in features) {
@@ -152,43 +159,45 @@ let sketch = function (p5) {
         console.log(key, ': ', features[key]);
       }
     }
-
-    draw_bg();
-    draw_star_body();
+    show_progress(1);
+    draw_bg(buffer);
+    show_progress(5);
+    draw_star_body(buffer);
+    show_progress(10);
   }
 
   // Draw dark twinkling star background
-  function draw_bg() {
-    p5.background('black');
+  function draw_bg(b) {
+    b.background('black');
 
-    p5.push();
+    b.push();
     // Translate to upper left corner to use regular x,y coordinates
-    p5.translate(-width / 2, -height / 2);
+    b.translate(-b.width / 2, -b.height / 2);
 
     // Dark but not black, and slightly transparent
-    p5.noStroke()
-    p5.fill(240, 80, 10, 0.8);
-    p5.rect(0, 0, width, height);
+    b.noStroke()
+    b.fill(240, 80, 10, 0.8);
+    b.rect(0, 0, b.width, b.height);
 
     // Compute number of background stars
-    let bg_point_count = p5.floor(width * height * 0.0025);
+    let bg_point_count = Math.floor(b.width * b.height * 0.0025);
 
     // Draw each background star
     for (let i = 0; i < bg_point_count; i++) {
-      let x = fxrand() * width;
-      let y = fxrand() * height;
-      if (i < bg_point_count * 0.9) p5.stroke(0, 0, p5.map(fxrand(), 0, 1, 35, 70), 0.9);
-      else p5.stroke(240, 90, p5.map(fxrand(), 0, 1, 50, 100), 0.9);
-      p5.strokeWeight(2);
-      p5.point(x, y);
+      let x = fxrand() * b.width;
+      let y = fxrand() * b.height;
+      if (i < bg_point_count * 0.9) b.stroke(0, 0, p5.map(fxrand(), 0, 1, 35, 70), 0.9);
+      else b.stroke(240, 90, p5.map(fxrand(), 0, 1, 50, 100), 0.9);
+      b.strokeWeight(2);
+      b.point(x, y);
       point_count++;
     }
-    p5.pop();
+    b.pop();
   }
 
-  function draw_arcs(iter) {
-    p5.noFill();
-    p5.strokeWeight(2);
+  function draw_arcs(b, iter) {
+    b.noFill();
+    b.strokeWeight(2);
 
     let hue = palettes[palette][1];
 
@@ -198,22 +207,22 @@ let sketch = function (p5) {
     let start_t = ((iter - 1) * batch_size) / arc_point_count;
     let end_t = iter * batch_size / arc_point_count;
 
-    p5.push();
-    let len = width * 0.5;
+    b.push();
+    let len = b.width * 0.5;
     for (let i = 0; i < arc_count; i++) {
       for (let t = start_t; t < end_t; t += 1 / arc_point_count) {
-        let x = p5.bezierPoint(-len * start_off, len, len, len * end_x_off, t);
-        let y = p5.bezierPoint(-len * start_off, 0, len, len * end_y_off, t);
+        let x = b.bezierPoint(-len * start_off, len, len, len * end_x_off, t);
+        let y = b.bezierPoint(-len * start_off, 0, len, len * end_y_off, t);
 
-        if (x < width && y < height) {
+        if (x < b.width && y < b.height) {
           // Fade colors based on distance to center
-          let fade = 1.2 * Math.floor(p5.dist(x, y, 0, 0), 0, width * 1.1, 1, 0);
-          p5.stroke(hue, 80, 100 * fade, 0.9);
+          let fade = 1.2 * Math.floor(b.dist(x, y, 0, 0), 0, b.width * 1.1, 1, 0);
+          b.stroke(hue, 80, 100 * fade, 0.9);
 
           // Perturb the location a bit, based on distance to center
-          let amplitude = p5.map(p5.dist(x, y, 0, 0), 0, width * 1.2, 1, max_point_amplitude);
+          let amplitude = p5.map(b.dist(x, y, 0, 0), 0, b.width * 1.2, 1, max_point_amplitude);
           let off = amplitude * p5.map(fxrand(), 0, 1, -1, 1);
-          p5.point(x + off, y - off);
+          b.point(x + off, y - off);
           point_count++;
         } else {
           early_stop = true;
@@ -221,21 +230,21 @@ let sketch = function (p5) {
         }
       }
 
-      p5.rotate(360 / arc_count)
+      b.rotate(360 / arc_count)
       if (early_stop) break;
     }
-    p5.pop();
+    b.pop();
   }
 
-  function draw_star(iter) {
+  function draw_star(b, iter) {
     if (iter > max_star_iter) return;
 
-    p5.noFill();
-    p5.strokeWeight(2);
+    b.noFill();
+    b.strokeWeight(2);
 
     let hue = palettes[palette][0];
 
-    p5.push();
+    b.push();
     let start_t;
     let end_t;
 
@@ -244,52 +253,57 @@ let sketch = function (p5) {
 
     for (let i = 0; i < star_repeat; i++) {
       // Make length vary a bit
-      let len = width * 0.2 + p5.map(fxrand(), 0, 1, -width * 0.05, width * star_ray_len);
+      let len = b.width * 0.2 + p5.map(fxrand(), 0, 1, -b.width * 0.05, b.width * star_ray_len);
 
       // Draw a portion determined by 'iter' argument
       end_t = p5.map(iter, 0, max_star_iter, 0, 1);
       start_t = Math.max(0, end_t - 1/max_star_iter);
 
       for (let t = start_t; t < end_t; t += 1 / star_point_count) {
-        let x = p5.bezierPoint(start_off, 0, 0, len, t);
-        let y = p5.bezierPoint(start_off, 0, 0, len, t);
+        let x = b.bezierPoint(start_off, 0, 0, len, t);
+        let y = b.bezierPoint(start_off, 0, 0, len, t);
         if (x < skip_radius && y < skip_radius) continue;
 
         // Fade colors based on distance to center
-        let fade = p5.map(p5.dist(0, 0, x, y), 0, width / 2, 1, 0.4);
-        p5.stroke(hue, 90, 90 * fade, 0.6);
+        let fade = p5.map(b.dist(0, 0, x, y), 0, b.width / 2, 1, 0.4);
+        b.stroke(hue, 90, 90 * fade, 0.6);
 
         // Perturb the location a bit, based on distance to center
-        let amplitude = 1.2 * p5.map(p5.dist(0, 0, x, y), 0, width * 1.1, 2, 8);
+        let amplitude = 1.2 * p5.map(p5.dist(0, 0, x, y), 0, b.width * 1.1, 2, 8);
         let off = amplitude * p5.map(fxrand(), 0, 1, -1, 1);
-        p5.point(x + off, y + off);
+        b.point(x + off, y + off);
         point_count++;
       }
-      p5.rotate(360 / star_repeat)
+      b.rotate(360 / star_repeat)
     }
-    p5.pop();
+    b.pop();
   }
 
-  function draw_star_body() {
+  function draw_star_body(b) {
     // Draw solid star body in same color as star light rays
-    star_size_px_min = Math.floor(width/20);
-    star_size_px_max = Math.floor(width/4);
+    star_size_px_min = Math.floor(b.width/20);
+    star_size_px_max = Math.floor(b.width/4);
     star_size_px = p5.map(start_off, 0.03, 0.4, star_size_px_min, star_size_px_max);
 
     if (star) {
-      p5.fill(palettes[palette][0], 90, 90, 0.8);
-      p5.circle(0, 0, star_size_px);
+      b.fill(palettes[palette][0], 90, 90, 0.8);
+      b.circle(0, 0, star_size_px);
     }
   }
 
   p5.draw = function () {
-    if (star) draw_star(p5.frameCount);
+    let progress = Math.round(p5.map(p5.frameCount, 1, max_star_iter, 10, 100));
+    show_progress(progress);
+
+    if (star) draw_star(buffer, p5.frameCount);
     if (p5.frameCount > max_star_iter) {
-      draw_arcs(p5.frameCount - max_star_iter);
+      draw_arcs(buffer, p5.frameCount - max_star_iter);
+      show_buffer(buffer);
     }
 
     if (p5.frameCount >= (max_arch_iter+max_star_iter) || early_stop) {
       // end animation and call fxpreview
+      show_buffer(buffer);
       p5.noLoop();
       fxpreview();
 
@@ -298,13 +312,35 @@ let sketch = function (p5) {
     }
   }
 
+  // Show the buffer on screen
+  function show_buffer(b) {
+    p5.push();
+    let x = 0;
+    let y = 0;
+    if (width*fullscreen_ratio > height) {
+      y = -(width*fullscreen_ratio - height)/2
+    }
+    p5.image(b, x, y, width, width*fullscreen_ratio);
+    p5.pop();
+  }
+
+  function show_progress(amount) {
+    p5.background('black');
+    p5.textFont('Verdana');
+    p5.textAlign(p5.CENTER);
+    p5.textSize(48);
+    p5.fill('white');
+    p5.stroke('white');
+    p5.text('Drawing star... ' + amount + '%', width/2, height/2);
+  }
+
   // function to save an output with the unique hash as the filename,
   // when the user presses 's' (upper or lower-case)
   p5.keyTyped = function (e) {
     const keyS = 83;
     const keys = 115;
     if (e.keyCode === keyS || e.keyCode === keys) {
-      p5.saveCanvas(fxhash, 'png');
+      buffer.save(fxhash + '_' + buffer.width + 'x' + buffer.height, 'jpg');
     }
     return false; // prevent any unwanted default browser behaviour
   }
@@ -314,14 +350,7 @@ let sketch = function (p5) {
     height = window.innerHeight;
     p5.resizeCanvas(width, height);
     console.log('Resized to', width, 'x', height);
-
-    start = Date.now();
-    p5.frameCount = 1;
-    early_stop = false;
-    draw_bg();
-    draw_star_body();
-    p5.loop();
-    p5.draw();
+    show_buffer(buffer);
   }
 }
 
